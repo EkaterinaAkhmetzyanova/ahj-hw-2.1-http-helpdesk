dfc47c1ec5f29307f692a7c01a679368292c3022
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-07-31 15:31:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/n55q2jhsipbgidni?svg=true)](https://ci.appveyor.com/project/EkaterinaAkhmetzyanova/ahj-hw-2-1-http-helpdesk)
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import './js/app';
+import './css/style.css';
+import './js/ticketsWidget';
+import './js/Tickets';
+import './js/request';
+import './js/Modal';
+import './js/DeleteModal';
